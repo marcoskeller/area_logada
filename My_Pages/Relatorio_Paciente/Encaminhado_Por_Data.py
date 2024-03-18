5c3edf514569baf421af877f3beaf6968c1e15af
Marcos Keller <marcoskeller@hotmail.com>
2024-03-18 11:37:30 -0300

    Atualizacao da Base de Dados dos Pacientes Encaminhados

--- My_Pages/Relatorio_Paciente/Encaminhado_Por_Data.py
+++ My_Pages/Relatorio_Paciente/Encaminhado_Por_Data.py
@@ -11,7 +11,7 @@ def gerar_df():
         engine="openpyxl",
         sheet_name="Encaminhamento Imuno",
         usecols="A:H",
-        nrows=44
+        nrows=46
 
     )
     return df
--- My_Pages/Relatorio_Paciente/Encaminhado_Por_Praca.py
+++ My_Pages/Relatorio_Paciente/Encaminhado_Por_Praca.py
@@ -12,7 +12,7 @@ def gerar_df():
         engine="openpyxl",
         sheet_name="Encaminhamento Imuno",
         usecols="A:H",
-        nrows=44
+        nrows=46
 
     )
     return df
