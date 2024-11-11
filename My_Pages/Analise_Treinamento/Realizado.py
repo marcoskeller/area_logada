603e3a5c822eb11d9aa814185220687206834da6
Marcos Keller <marcoskeller@hotmail.com>
2024-11-11 18:17:16 -0300

    Atualizacao Pagina Realizado - Retirando o Filtro de Contato com o Médico

--- My_Pages/Analise_Treinamento/Realizado.py
+++ My_Pages/Analise_Treinamento/Realizado.py
@@ -77,13 +77,20 @@ def treinamentoRealizadoPorData(data):
         (df['Treinamento'] == "REALIZADO")
     ]
 
+    
+
     #Conversao de Data
     dadosUsuario["Data da realização do Treinamento"] = pd.to_datetime(df["Data da realização do Treinamento"], errors='coerce',utc=False)
     dadosUsuario['Data da realização do Treinamento'] = dadosUsuario['Data da realização do Treinamento'].dt.strftime('%b/%Y')
     dadosUsuario = dadosUsuario.sort_values("Data da realização do Treinamento")
 
-    #Quantidade de Treinamento Por Consultor
-    fig_kind = px.pie(dadosUsuario, values="Quantidade Realizada", names="Data da realização do Treinamento",
+
+    ##Contribuição por Estado
+    quantidade_mes = dadosUsuario.groupby("Data da realização do Treinamento")[["Quantidade Realizada"]].sum().reset_index()
+
+    #Quantidade de Treinamento Por Mes
+    #dadosUsuario = dadosUsuario.groupby("Praça")[["Quantidade Realizada"]].sum().reset_index()
+    fig_kind = px.bar(quantidade_mes, x="Data da realização do Treinamento", y="Quantidade Realizada",
     title="Treinamento(s) Realizado(s) Por Mês Até o Presente")
     
     if data == "2 - Exibir":
@@ -118,86 +125,6 @@ def totalTreinamentoRealizados(status):
     if status == "2 - Exibir":
         # Exibir o gráfico
         st.plotly_chart(fig_city, use_container_width=True)   
-    else:
-        print()
-
-def contatoPosTreinamento(contato):
-    #Chama funçao que obtem os dados
-    df = gerar_df()
-
-    #Seleciona somente as colunas uteis
-    colunaUteis = ['Nome do Médico', 'Consultor/GO responsável','Praça', 'Data da realização do Treinamento',
-        'Treinamento', 'Objeção para Realizar Parte Administrativa', 'Contato Medico Apos treinamento', 'Onboarding Finalizado', 'Quantidade Realizada']
-    
-    #Cria um dataFrame com as colunas uteis
-    df = df[colunaUteis] 
-
-    dadosUsuario = df
-
-    dadosUsuario["Data da realização do Treinamento"] = pd.to_datetime(df["Data da realização do Treinamento"], errors='coerce',utc=False)
-    dadosUsuario['Data da realização do Treinamento'] = dadosUsuario['Data da realização do Treinamento'].dt.strftime('%m/%Y')
-
-
-    if contato == "2 - Exibir":
-        dadosUsuario = df.loc[(
-            df['Contato Medico Apos treinamento'] == "SIM")
-        ]
-        
-        # Exibir o DataFrame filtrado
-        st.dataframe(dadosUsuario, use_container_width=True, hide_index=True)
-        st.write( "Total Médicos Contactados: " , len(dadosUsuario))
-
-        #Filtro da Praça dos Medicos Selecionadaos
-        dadosUsuario = df.loc[(
-            df['Contato Medico Apos treinamento'] == "SIM")
-        ]
-
-
-        #Criar o gráfico de barras para exibir o quantidade de medicos que receberam contato
-        city_total = dadosUsuario.groupby("Praça")[["Quantidade Realizada"]].sum().reset_index()
-        fig_city = px.bar(city_total, x="Praça", y="Quantidade Realizada",
-        title="Quantidade Contato(s) Realizado(s) Pós Treinamento(s) Realizado Por Praça")
-        st.plotly_chart(fig_city, use_container_width=True)    
-    else:
-        print()
-
-def contatoNaoRealizadoPosTreinamento(contato):
-    #Chama funçao que obtem os dados
-    df = gerar_df()
-
-    #Seleciona somente as colunas uteis
-    colunaUteis = ['Nome do Médico', 'Consultor/GO responsável','Praça', 'Data da realização do Treinamento',
-        'Treinamento', 'Contato Medico Apos treinamento', 'Onboarding Finalizado', 'Quantidade Realizada']
-    
-    #Cria um dataFrame com as colunas uteis
-    df = df[colunaUteis] 
-
-    dadosUsuario = df
-
-    dadosUsuario["Data da realização do Treinamento"] = pd.to_datetime(df["Data da realização do Treinamento"], errors='coerce',utc=False)
-    dadosUsuario['Data da realização do Treinamento'] = dadosUsuario['Data da realização do Treinamento'].dt.strftime('%m/%Y')
-
-
-    if contato == "2 - Exibir":
-        
-        dadosUsuario = df.loc[(
-            df['Treinamento'] == "REALIZADO") &
-            (df['Contato Medico Apos treinamento'] == "NÃO")
-        ]
-        
-        # Exibir o DataFrame filtrado
-        st.dataframe(dadosUsuario, use_container_width=True, hide_index=True)
-        st.write( "Total Médico(s) que não foram Contactados Pós Treinamento: " , len(dadosUsuario))
-
-        dadosUsuario = df.loc[(
-            df['Contato Medico Apos treinamento'] == "NÃO")
-        ]
-
-        #Criar o gráfico de barras para exibir o quantidade de medicos que receberam contato
-        city_total = dadosUsuario.groupby("Praça")[["Quantidade Realizada"]].sum().reset_index()
-        fig_city = px.bar(city_total, x="Praça", y="Quantidade Realizada",
-        title="Quantidade de Médico(s)Que Não Receberam Contato Pós Treinamento")
-        st.plotly_chart(fig_city, use_container_width=True)    
     else:
         print()
 
@@ -285,12 +212,6 @@ def filtroUteis():
     #Recebe o filtro que iremos utilizar
     treinamentoRealizadoPorData(st.selectbox("Total de Treinamento(s) Realizado(s) Por Mês Até o Momento",opcaoData))
    
-    #Recebe o filtro que iremos utilizar
-    contatoPosTreinamento(st.selectbox("Total de Médico(s) que foram contactados Pós Treinamento", options=opcaoData))
-
-    #Recebe o filtro que iremos utilizar
-    contatoNaoRealizadoPosTreinamento(st.selectbox("Total de Médico(s) que não foram contactados Pós Treinamento", options=opcaoData))  
-
 def paginaRealizadoInicial():
     filtroUteis()
 
